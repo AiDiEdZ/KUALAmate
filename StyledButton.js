4f441c5cd058f7c31ef22b701aeb8ede70b704cc
AiDiEdZ <123794739+AiDiEdZ@users.noreply.github.com>
2023-02-28 12:40:21 +0800

    Update StyledButton.js

--- StyledButton.js
+++ StyledButton.js
@@ -1,26 +1,43 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Button = styled.button`
-  font-size: 1em;
-  margin: 1em;
-  padding: 0.25em 1em;
-  border-radius: 3px;
+const ComingSoonContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  width: 100%;
+`;
 
-  /* Color the border and text with theme.main */
-  color: ${props => props.theme.main};
-  border: 2px solid ${props => props.theme.main};
+const Heading = styled.h1`
+  font-size: 3rem;
+  margin-bottom: 2rem;
+`;
+
+const Text = styled.p`
+  font-size: 1.5rem;
+  margin-bottom: 2rem;
 `;
 
 const StyledButton = styled.button`
-  font-size: 1em;
-  margin: 1em;
-  padding: 0.25em 1em;
-  border-radius: 3px;
-
-  /* Color the border and text with theme.main */
-  color: ${props => props.theme.main};
-  border: 2px solid ${props => props.theme.main};
+  font-size: 1.5rem;
+  padding: 1rem 2rem;
+  border: none;
+  border-radius: 0.5rem;
+  background-color: ${props => props.theme.main};
+  color: ${props => props.theme.white};
+  cursor: pointer;
 `;
 
-export default StyledButton;
+function ComingSoonPage() {
+  return (
+    <ComingSoonContainer>
+      <Heading>Coming Soon!</Heading>
+      <Text>Our new website will allow you to place furniture and textiles within your house using a VR feature. We can't wait to share it with you!</Text>
+      <StyledButton>Back to KUALAmate</StyledButton>
+    </ComingSoonContainer>
+  );
+}
+
+export default ComingSoonPage;
